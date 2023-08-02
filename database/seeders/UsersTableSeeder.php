b562c192403c6255cbfc5aed08b75a986c3cf8ef
gommadigitale <gommadigitale@gmail.com>
2023-08-02 10:13:51 +0200

    user seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -11,18 +11,13 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'John',
-            'email' => 'john@corsi.it',
-            'password' => Hash::make('password'),
-        ]);
-        User::create([
-            'name' => 'Jane',
-            'email' => 'jane@corsi.it',
-            'password' => Hash::make('password'),
-        ]);
-        User::create([
             'name' => 'Salvi',
             'email' => 'salvi@corsi.it',
+            'password' => Hash::make('password'),
+        ]);
+        User::create([
+            'name' => 'Tester',
+            'email' => 'tester@corsi.it',
             'password' => Hash::make('password'),
         ]);
     }
