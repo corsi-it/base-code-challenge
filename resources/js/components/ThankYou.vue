a4a66b124223e3b5bbaf2b9c807af8e0226c37f2
gommadigitale <gommadigitale@gmail.com>
2023-08-02 10:11:13 +0200

    thankyou view styled

--- resources/js/components/ThankYou.vue
+++ resources/js/components/ThankYou.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-      <h1>Thank You</h1>
-      <p>Thank you for submitting your review!</p>
-      <router-link to="/dashboard">Back to Dashboard</router-link>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 shadow-md rounded-lg w-96">
+      <h1 class="text-3xl font-semibold mb-6">Thank You</h1>
+      <p class="mb-6">Thank you for submitting your review!</p>
+      <router-link to="/dashboard" class="text-blue-500 hover:underline">Back to Dashboard</router-link>
     </div>
+  </div>
 </template>
