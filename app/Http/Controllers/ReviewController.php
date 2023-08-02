7da08e811e8c169883b6cb1bef1c5ff7d77ed728
gommadigitale <gommadigitale@gmail.com>
2023-08-02 10:13:39 +0200

    added forbidden word validation

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,23 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ReviewController extends Controller
 {
     public function index()
     {
-        $reviews = Review::all();
+        $reviews = Review::with('employee')->get();
         return response()->json($reviews);
     }
     
     public function store(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'employee_id' => 'required|exists:users,id',
             'rating' => 'required|integer|min:1|max:5',
-            'review' => 'required|string',
+            'review' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    $forbiddenWords = ['Boring', 'ugly', 'smelly', 'shitty', '.NET', 'bullshit', 'he owes me money'];
+                    foreach ($forbiddenWords as $word) {
+                        if (stripos($value, $word) !== false) {
+                            $fail("The review contains unkind words");
+                        }
+                    }
+                },
+            ],
         ]);
-    
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors(), 'status'=> 0], 422);
+        }
         $review = new Review();
         $review->employee_id = $request->input('employee_id');
         $review->rating = $request->input('rating');
@@ -31,6 +45,6 @@ class ReviewController extends Controller
         
         $review->save();
 
-        return response()->json($review, 201);
+        return response()->json(['message' => 'Review correctly added','status' => 1], 201);
     }
 }
