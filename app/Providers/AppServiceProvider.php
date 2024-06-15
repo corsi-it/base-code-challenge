6d061f359583c1633124e6b373e4645eb3f05b5f
Michele Capichioni <capimichi@gmail.com>
2024-06-15 19:55:54 +0200

    Add StockRequestsHelper class for managing stock request dates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Helpers\ItemsHelper;
+use App\Helpers\StockRequestsHelper;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(ItemsHelper::class, function () {
             return new ItemsHelper();
+        });
+
+        $this->app->bind(StockRequestsHelper::class, function () {
+            return new StockRequestsHelper();
         });
     }
 
