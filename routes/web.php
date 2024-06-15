1d321a19dd441d38c8f68553eb295d7b5191d80f
Michele Capichioni <capimichi@gmail.com>
2024-06-15 11:29:31 +0200

    Login web route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', 'App\Http\Controllers\Web\AuthController@login')->name('login');
