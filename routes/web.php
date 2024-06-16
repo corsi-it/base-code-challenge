dca578bb64f31f145d8f03d77bdb0ce75dcbf1b0
Michele Capichioni <capimichi@gmail.com>
2024-06-16 19:04:23 +0200

    Refactor routes to include logout route and organize dashboard and stock request routes

--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\Web\DefaultController@index')->name('home');
-Route::get('/login', 'App\Http\Controllers\Web\AuthController@login')->name('login');
-Route::post('/login', 'App\Http\Controllers\Web\AuthController@loginStore')->name('login.store');
+Route::get('/', 'App\Http\Controllers\DefaultController@index')->name('home');
+Route::get('/login', 'App\Http\Controllers\AuthController@login')->name('login');
+Route::post('/login', 'App\Http\Controllers\AuthController@loginStore')->name('login.store');
 
 // Group routes that only require 'auth' middleware
 Route::middleware(['auth'])->group(function () {
-    Route::get('/logout', 'App\Http\Controllers\Web\AuthController@logout')->name('logout');
-    Route::get('/dashboard/stock-requests', 'App\Http\Controllers\Web\StockRequestsController@index')->name('stockRequests.index');
-    Route::get('/dashboard/stock-requests/create', 'App\Http\Controllers\Web\StockRequestsController@create')->name('stockRequests.create');
-    Route::post('/dashboard/stock-requests', 'App\Http\Controllers\Web\StockRequestsController@store')->name('stockRequests.store');
-    Route::get('/dashboard/stock-requests/{id}', 'App\Http\Controllers\Web\StockRequestsController@show')->name('stockRequests.show');
-    Route::get('/dashboard/stock-requests/{id}/edit', 'App\Http\Controllers\Web\StockRequestsController@edit')->name('stockRequests.edit');
+    Route::get('/logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
+    Route::get('/dashboard/stock-requests', 'App\Http\Controllers\StockRequestsController@index')->name('stockRequests.index');
+    Route::get('/dashboard/stock-requests/create', 'App\Http\Controllers\StockRequestsController@create')->name('stockRequests.create');
+    Route::post('/dashboard/stock-requests', 'App\Http\Controllers\StockRequestsController@store')->name('stockRequests.store');
+    Route::get('/dashboard/stock-requests/{id}', 'App\Http\Controllers\StockRequestsController@show')->name('stockRequests.show');
+    Route::get('/dashboard/stock-requests/{id}/edit', 'App\Http\Controllers\StockRequestsController@edit')->name('stockRequests.edit');
 });
 
 // Group routes that require both 'auth' and 'role.admin' middleware
 Route::middleware(['auth', 'role.admin'])->group(function () {
-    Route::get('/dashboard', 'App\Http\Controllers\Web\DashboardController@index')->name('dashboard');
-    Route::get('/dashboard/charts/user-requests', 'App\Http\Controllers\Web\ChartController@userRequests')->name('charts.userRequests');
-    Route::get('/dashboard/charts/item-requests', 'App\Http\Controllers\Web\ChartController@itemRequests')->name('charts.itemRequests');
-    Route::get('/dashboard/items', 'App\Http\Controllers\Web\ItemsController@index')->name('items.index');
-    Route::get('/dashboard/items/create', 'App\Http\Controllers\Web\ItemsController@create')->name('items.create');
-    Route::post('/dashboard/items', 'App\Http\Controllers\Web\ItemsController@store')->name('items.store');
-    Route::get('/dashboard/items/{id}', 'App\Http\Controllers\Web\ItemsController@show')->name('items.show');
-    Route::get('/dashboard/items/{id}/edit', 'App\Http\Controllers\Web\ItemsController@edit')->name('items.edit');
-    Route::put('/dashboard/items/{id}', 'App\Http\Controllers\Web\ItemsController@update')->name('items.update');
-    Route::get('/dashboard/stock-requests/{id}/edit', 'App\Http\Controllers\Web\StockRequestsController@edit')->name('stockRequests.edit');
-    Route::put('/dashboard/stock-requests/{id}', 'App\Http\Controllers\Web\StockRequestsController@update')->name('stockRequests.update');
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+    Route::get('/dashboard/charts/user-requests', 'App\Http\Controllers\ChartController@userRequests')->name('charts.userRequests');
+    Route::get('/dashboard/charts/item-requests', 'App\Http\Controllers\ChartController@itemRequests')->name('charts.itemRequests');
+    Route::get('/dashboard/items', 'App\Http\Controllers\ItemsController@index')->name('items.index');
+    Route::get('/dashboard/items/create', 'App\Http\Controllers\ItemsController@create')->name('items.create');
+    Route::post('/dashboard/items', 'App\Http\Controllers\ItemsController@store')->name('items.store');
+    Route::get('/dashboard/items/{id}', 'App\Http\Controllers\ItemsController@show')->name('items.show');
+    Route::get('/dashboard/items/{id}/edit', 'App\Http\Controllers\ItemsController@edit')->name('items.edit');
+    Route::put('/dashboard/items/{id}', 'App\Http\Controllers\ItemsController@update')->name('items.update');
+    Route::get('/dashboard/stock-requests/{id}/edit', 'App\Http\Controllers\StockRequestsController@edit')->name('stockRequests.edit');
+    Route::put('/dashboard/stock-requests/{id}', 'App\Http\Controllers\StockRequestsController@update')->name('stockRequests.update');
 });
