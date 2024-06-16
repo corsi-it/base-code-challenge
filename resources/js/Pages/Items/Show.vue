eac9ebc249fa029b2febb114fcb39f6cdb806a19
Michele Capichioni <capimichi@gmail.com>
2024-06-16 19:04:19 +0200

    Add "Available" section to item details page

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -18,6 +18,17 @@
                         </li>
                     </ul>
                 </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Available</label>
+                    <p class="text-gray-700 text-base">
+                        <span v-if="item.isInStock">
+                                        Available
+                                    </span>
+                        <span v-if="!item.isInStock">
+                                        Non Available
+                                    </span>
+                    </p>
+                </div>
             </div>
         </div>
 
